e84ce7e616903818d0190ad7232acc447203e62f
zeke <koteer212@gmail.com>
2024-11-10 09:00:17 -0600

    djfk

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Ensure you are importing the correct module
+import ReactDOM from 'react-dom/client';
 import App from './App';
-const rootElement = document.getElementById('root');
-if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-        <React.StrictMode>
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-    </React.StrictMode>
-); // Your main App component
-} else {
-    console.error('Root element not found');
-}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoit. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
