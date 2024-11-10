96d617174c5d19ccfa1ddbedad9fbf920914f2eb
zeke <koteer212@gmail.com>
2024-11-10 08:39:34 -0600

    dhfjf

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Ensure you are importing the correct module
-import App from '../src/App';
+import App from './App';
 const rootElement = document.getElementById('root');
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
