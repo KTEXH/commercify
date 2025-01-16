2193c20463c0a24a7222ba87774892ba2de822b8
zeke <koteer212@gmail.com>
2025-01-15 22:43:21 -0600

    dfkf

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -107,47 +107,49 @@ export default function LandingPage({ className = "" }) {
             <Bars3Icon onClick={() => setMobileMenuOpen(true)} class='text-black h-5' />
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
               <div className="fixed inset-0 z-10" />
-              <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <DialogPanel className="fixed inset-y-0 top-10 rounded-t-2xl flex flex-col justify-center items-center right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div className="flex items-center justify-between">
                   <a href="#" className="-m-1.5 p-1.5">
                     <span className="sr-only">Your Company</span>
-                    <Group className='w-7 h-7' />
 
                   </a>
-                  <button
-                    type="button"
-                    onClick={() => setMobileMenuOpen(false)}
-                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                  >
-                    <span className="sr-only">Close menu</span>
-                    <XMarkIcon aria-hidden="true" className="size-6" />
-                  </button>
+            
                 </div>
                 <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="py-3">
+                  <div className="-my-6">
+            
 
                       <a
                         href="#"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-['Semibold'] text-gray-900 hover:bg-gray-50"
+                        className="-mx-3 block rounded-lg px-3 py-2 text-center text-xl font-['Semibold'] text-gray-900 hover:bg-gray-50"
                       >
                         Plans
                       </a>
 
-                    </div>
-
-                    <div className="py-3 flex flex-col gap-2">
                       <a
                         href="#"
-                        className="-mx-3 block rounded-lg px-3 inline-flex py-2 text-base/7 font-['Semibold'] text-gray-900 hover:bg-gray-50"
+                        className="-mx-3 block rounded-lg px-3 py-2 text-center text-xl font-['Semibold'] text-gray-900 hover:bg-gray-50"
+                      >
+                        Socials
+                      </a>
+                    <div className="pb-3 flex flex-col gap-2">
+                      <a
+                        href="#"
+                        className="-mx-3 block rounded-lg text-xl text-center px-3 py-2  font-['Semibold'] text-gray-900 hover:bg-gray-50"
                       >
                         Login
                       </a>
                       <a
                         href="#"
-                        className="-mx-3 inline-block rounded-full text-white bg-black px-5 py-3 text-base/7 text-sm font-['Medium']"
+                        className="-mx-3 inline-block rounded-full text-white bg-black px-5 py-3 text-base/7 text-sm font-['Semibold']"
                       >
                         Get started with beta
+                      </a>
+                      <a
+                        href="#"
+                        className="-mx-3 inline-block rounded-full text-black bg-gray-100 text-center px-5 py-3 text-base/7 text-sm font-['Semibold']"
+                      >
+                        Close
                       </a>
                     </div>
                   </div>
