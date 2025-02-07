5ef9ad5c09e6aabddeb38f2c17477181ee9c082d
zeke <koteer212@gmail.com>
2025-02-06 22:07:31 -0600

    djfk

--- src/components/Beta.jsx
+++ src/components/Beta.jsx
@@ -3,12 +3,14 @@ import { Header } from './LandingHeader'
 
 export const Beta = () => {
     return(
-        <div class='lg:max-w-screen-lg lg:mx-auto mx-5 flex items-center justify-center flex-col'>
+        <div class='bg-black flex flex-col h-screen'>
+        <div class='lg:max-w-screen-lg lg:mx-auto h-full mx-5 flex items-center bg-black justify-center flex-col'>
             <Header />
             <div class='mt-32 flex justify-center flex-col'>
                  <div class='text-6xl font-["Semibold"]'>Beta</div>
                  <div class=''>Get used to beta and sign up early</div>
             </div>
+        </div>
         </div>
     )
 }
