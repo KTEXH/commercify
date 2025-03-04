f274d4dabe23247f0e99c6915e15818db583e1ca
zeke <koteer212@gmail.com>
2025-03-03 21:33:52 -0600

    dgkg

--- src/components/Beta.jsx
+++ src/components/Beta.jsx
@@ -1,80 +1,93 @@
+import Group from '../components/assets/Group'
 import React, { useState } from 'react'
-import { Header } from './LandingHeader'
-import Group from './assets/Group'
-import { Bars3Icon } from '@heroicons/react/20/solid'
-import { DialogPanel, Dialog } from '@headlessui/react'
+import { gql, useMutation } from '@apollo/client'
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
 
 export const Beta = () => {
 
+    const dont = [
+        'Musician', 'Barber', 'Ecom', 'Stylist', 'Brand', 'Artist', 'Other'
+    ]
 
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <div class='bg-[#111111] flex flex-col h-screen'>
-            <div class='lg:max-w-screen-lg lg:mx-auto h-full mx-5 flex  flex-col'>
-                <div class="bg-gray-600  overflow-hidden z-10 bg-opacity-65 backdrop-blur-md md:w-full w-96 sm:max-w-xl fixed top-6 sm:left-1/2 sm:transform sm:-translate-x-1/2 mx-2 sm:mx-auto flex items-center justify-between rounded-full px-4 sm:px-6 p-4 overflow-x-auto">
-                    <div class='flex items-center gap-2'>
-                        <Group className='w-7 h-7' />
-                        <div class='font-["Semibold"] text-white'>Commercify HQ</div>
-                    </div>
-                    <div class='items-center gap-7 md:flex hidden text-white font-["Semibold"] text-sm'>
-                        <a href='/pricing'>Pricing</a>
-                        <a href='/login'>Login</a>
-                    </div>
-                    <div class='sm:flex md:hidden'>
-                        <Bars3Icon onClick={() => setMobileMenuOpen(true)} class='text-black h-5' />
-                        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                            <div className="fixed inset-0 z-10" />
-                            <DialogPanel className="fixed inset-y-0 top-10 rounded-t-2xl flex flex-col justify-center items-center right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                                <div class='flex flex-col relative items-center ustify-center'>
-                                    <div className="fixed items-center justify-center flex flex-col top-16">
-                                        <Group className='w-7 h-7' />
+        <div class='w-full'>
+      <div class=' md:flex items-center md:justify-center relative'>
 
-                                    </div>
-                                    <div className="mt-6 flow-root">
-                                        <div className="-my-6">
+        <div className="flex min-h-full flex-1 flex-col md:justify-center px-6 py-12 lg:px-8">
+          <div className="md:flex items-center sm:justify-center flex-col">
+            <Group className='w-8 h-8' />
+            <h2 className="mt-7 md:text-center font-['Semibold'] text-3xl leading-9 tracking-tight text-black">
+              Get access to beta
+            </h2>
+          </div>
 
+          <div className="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6">
+              <div>
 
-                                            <a
-                                                href="/pricing"
-                                                className="-mx-3 block rounded-lg px-3 py-2 text-center text-3xl font-['Semibold'] text-gray-900 hover:bg-gray-50"
-                                            >
-                                                Pricing
-                                            </a>
-
-                                            <a
-                                                href="/socials"
-                                                className="-mx-3 mb-5 block rounded-lg px-3 py-2 text-center text-3xl font-['Semibold'] text-gray-900 hover:bg-gray-50"
-                                            >
-                                                Socials
-                                            </a>
-                                            <div className="pb-3 flex flex-col gap-2">
-
-                                                <a
-                                                    href="/beta"
-                                                    className="-mx-3 inline-block rounded-full text-white bg-black px-5 py-4 text-xl font-['Semibold']"
-                                                >
-                                                    Get started with beta
-                                                </a>
-                                                <a
-                                                    onClick={() => setMobileMenuOpen(false)}
-                                                    href="#"
-                                                    className="-mx-3 inline-block rounded-full text-black bg-gray-100 text-center px-5 py-4 text-xl font-['Semibold']"
-                                                >
-                                                    Close
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </DialogPanel>
-                        </Dialog>
-                    </div>
+                <div className="mt-2 gap-2 flex flex-col">
+                  <label class='font-["Semibold"] text-sm'>Name</label>
+                  <input
+                    id="name"
+                    name="name"
+                    placeholder='John'
+                    type="name"
+                    required
+                    autoComplete="name"
+                    className="block w-full px-3 border text-sm font-['Medium'] rounded-xl py-3 text-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
-                <div class='mt-32 flex justify-center text-white flex-col'>
-                    <div class='text-6xl font-["Semibold"]'>Get started with Beta</div>
+              </div>
+
+              <div>
+
+                <div className="mt-2 gap-2 flex flex-col">
+                  <label class='font-["Semibold"] text-sm'>Email</label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder='email@example.com'
+                    required
+                    autoComplete="email"
+                    className="block w-full rounded-xl text-sm font-['Medium'] border px-3 py-3 text-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
-            </div>
+               
+              </div>
+
+
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full rounded-full justify-center bg-black px-3 py-3 font-['Semibold'] leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get access
+                </button>
+
+                <a
+                  href='/login'
+                  className="flex mt-2 w-full rounded-full justify-center border px-3 py-3 font-['Semibold'] leading-6 text-black hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </a>
+              </div>
+
+              <div class='text-gray-400 text-center text-xs font-["Medium"]'>By signing up you are agreeing to our <div class='text-black font-["Semibold"]'>terms of use</div></div>
+                <div class='rounded-lg mt-10 text-gray-400 border text-center p-2 bottom-5 text-xs font-["Medium"]'>
+                  <div>  By continuing to browse our site you are accepting our <a class='font-["Semibold"] text-black'>cookie policy</a></div>
+                </div>
+            </form>
+
+
+          </div>
         </div>
+      </div>
+
+    </div>
     )
 }
