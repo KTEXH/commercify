515c507dca157870e6b1864565d241970fb5b6d8
zeke <koteer212@gmail.com>
2025-03-13 17:41:26 -0500

    djfk

--- src/Pages/Pages/Pricing.jsx
+++ src/Pages/Pages/Pricing.jsx
@@ -3,6 +3,7 @@ import { Header } from '../../components/LandingHeader'
 import { CheckIcon } from '@heroicons/react/20/solid'
 import { Tab, TabGroup, TabList, TabPanel } from '@headlessui/react'
 import { useNavigate } from 'react-router-dom'
+import initials from '../../components/assets/initial.svg'
 import Group3 from '../../components/assets/Group3'
 const tiers = [
     {
@@ -12,21 +13,54 @@ const tiers = [
         priceMonthly: 'Free',
         priceYearly: 'Free',
         description: "The perfect plan if you're just getting started with our product.",
-        features: ['25 products', 'Up to 10,000 subscribers', 'Advanced analytics', '24-hour support response time'],
+        features: [
+            '5 products',
+            '1 Storefront',
+            '1 Workshop',
+            '1 Link-in-bio',
+            '1 Form',
+            '6.5% Transaction Fee',
+            'Link-in-bio tools'],
         featured: false,
+    },
+    {
+        name: 'Plus',
+        id: 'tier-enterprise',
+        href: '/beta',
+        priceMonthly: '$7',
+        priceYearly: '$76',
+        description: 'Perfect for creators and businesses any size.',
+        features: [
+            '10 products',
+            'No transaction fees',
+            '1 Storefront',
+            '1 Workshop',
+            '1 Link-in-bio',
+            '1 Form',
+            'Advanced analytics',
+            'Passcoded storefronts',
+            'Custom Domains',
+        ],
+        featured: true,
     },
     {
         name: 'Pro',
         id: 'tier-enterprise',
         href: '/beta',
-        priceMonthly: '$8',
+        priceMonthly: '$24',
         priceYearly: '$76',
-        description: 'Dedicated support and infrastructure for your company.',
+        description: 'Dedicated support and infrastructure for your company or prime-time creators.',
         features: [
             'Unlimited products',
-            'Unlimited subscribers',
+            'Unlimited Storefronts',
+            'Unlimited Workshops',
+            'Unlimited Link-in-bios',
+            'Unlimited Forms',
+            'No transaction fees',
             'Advanced analytics',
-            'Dedicated support representative',
+            'Passcoded storefronts',
+            'Custom form support',
+            'Branding options'
         ],
         featured: true,
     },
@@ -57,9 +91,8 @@ export function Pricing() {
                         />
                     </div>
                     <div className="mx-auto max-w-4xl text-center">
-                        <h2 className="text-base/7 font-['Semibold']">Pricing</h2>
                         <p className="mt-2 text-5xl font-['Semibold'] tracking-tight text-balance text-black sm:text-6xl">
-                            Choose the right plan for you
+                            Simple Pricing
                         </p>
                         <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-['Medium'] text-gray-500">
                             Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer
@@ -67,125 +100,101 @@ export function Pricing() {
                         </p>
                     </div>
                     <div>
-                        <TabGroup class='flex flex-col w-full justify-center items-center mt-5'>
-                            <TabList class='bg-gray-100 rounded-full px-2 py-1'>
-                                {time.map(time => {
-                                    return (
-                                        <Tab
-                                            className='data-[selected]:bg-white rounded-full px-3 text-gray-400 py-1 data-[selected]:text-md text-sm outline-0 data-[selected]:text-black font-["Medium"] data-[selected]:font-["Semibold"]'
-                                        >
-                                            {time}
-                                        </Tab>
-                                    )
-                                })}
-                            </TabList>
-                            <TabPanel>
 
-                                <div className="mx-auto grid max-w-lg grid-cols-1 gap-y-5 items-center gap-x-6 mt-7 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-                                    {tiers.map((tier) => (
-                                        <div
-                                            key={tier.id}
-                                            className={classNames(
-                                                'border rounded-xl p-7'
-                                            )}
-                                        >
-                                            <h3
-                                                id={tier.id}
-                                                className={classNames('font-["Semibold"] text-2xl')}
-                                            >
-                                                {tier.name}
-                                            </h3>
-                                            <p className="mt-4 flex items-baseline gap-x-2">
-                                                <span
-                                                    className={classNames('font-["Semibold"] text-2xl')}
-                                                >
-                                                    {tier.priceMonthly}
-                                                </span>
-                                                <span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-500', 'font-["Medium"]')}>/month</span>
-                                            </p>
-                                            <p className={classNames('mt-6 font-["Medium"] text-sm mb-4 text-base/7')}>
-                                                {tier.description}
-                                            </p>
-                                            <div
-                                                className={classNames('space-y-2')}
-                                            >
-                                                {tier.features.map((feature) => (
-                                                    <li key={feature} className="flex font-['Medium'] text-sm gap-x-3">
-                                                        <CheckIcon
-                                                            aria-hidden="true"
-                                                            className={classNames('h-5 w-4 text-gray-400 flex-none')}
-                                                        />
-                                                        {feature}
-                                                    </li>
-                                                ))}
-                                            </div>
-                                            <div
-                                                href={tier.href}
-                                                aria-describedby={tier.id}
-                                                className={classNames(tier.featured === false ? 'border text-black font-["Semibold"]' : 'bg-black font-["Semibold"] text-white', 'w-full rounded-full mt-4 text-sm flex items-center justify-center py-3')}
-                                            >
-                                                Get started today
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </TabPanel>
-                            <TabPanel>
 
-                                <div className="mx-auto grid max-w-lg grid-cols-1 gap-y-5 items-center gap-x-6 mt-7 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-                                    {tiers.map((tier) => (
-                                        <div
-                                            key={tier.id}
-                                            className={classNames(
-                                                'border rounded-xl p-7'
-                                            )}
+                        <div className="mx-auto grid max-w-lg grid-cols-1 gap-y-5 gap-x-6 mt-7 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-3">
+                            {tiers.map((tier) => (
+                                <div
+                                    key={tier.id}
+                                    className={classNames(
+                                        'border rounded-xl p-7'
+                                    )}
+                                >
+                                    <h3
+                                        id={tier.id}
+                                        className={classNames('font-["Semibold"] text-2xl')}
+                                    >
+                                        {tier.name}
+                                    </h3>
+                                    <p className="mt-4 flex items-baseline gap-x-2">
+                                        <span
+                                            className={classNames('font-["Bold"] text-2xl')}
                                         >
-                                            <h3
-                                                id={tier.id}
-                                                className={classNames('font-["Semibold"] text-2xl')}
-                                            >
-                                                {tier.name}
-                                            </h3>
-                                            <p className="mt-4 flex items-baseline gap-x-2">
-                                                <span
-                                                    className={classNames('font-["Semibold"] text-2xl')}
-                                                >
-                                                    {tier.priceYearly}
-                                                </span>
-                                                <span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-500', 'font-["Medium"]')}>/yearly</span>
-                                            </p>
-                                            <p className={classNames('mt-6 font-["Medium"] text-sm mb-4 text-base/7')}>
-                                                {tier.description}
-                                            </p>
-                                            <div
-                                                className={classNames('space-y-2')}
-                                            >
-                                                {tier.features.map((feature) => (
-                                                    <li key={feature} className="flex font-['Medium'] text-sm gap-x-3">
-                                                        <CheckIcon
-                                                            aria-hidden="true"
-                                                            className={classNames('h-5 w-4 text-gray-400 flex-none')}
-                                                        />
-                                                        {feature}
-                                                    </li>
-                                                ))}
-                                            </div>
-                                            <div
-                                                href={tier.href}
-                                                aria-describedby={tier.id}
-                                                className={classNames(tier.featured === false ? 'border text-black font-["Semibold"]' : 'bg-black font-["Semibold"] text-white', 'w-full rounded-full mt-4 text-sm flex items-center justify-center py-3')}
-                                            >
-                                                Get started today
-                                            </div>
-                                        </div>
-                                    ))}
+                                            {tier.priceMonthly}
+                                        </span>
+                                        <span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-400', 'font-["Medium"]')}>/mo</span>
+                                    </p>
+                                    <p className={classNames('mt-3 font-["Medium"] text-gray-400 text-sm mb-4')}>
+                                        {tier.description}
+                                    </p>
+                                    <div
+                                        className={classNames('space-y-2')}
+                                    >
+                                        {tier.features.map((feature) => (
+                                            <li key={feature} className="flex font-['Semibold'] text-sm gap-x-3">
+                                                <div class='bg-[#00b600] w-7 h-7 rounded-full flex items-center justify-center'>
+                                                    <CheckIcon
+                                                        aria-hidden="true"
+                                                        className={classNames('h-5 w-4 text-white flex-none')}
+                                                    />
+                                                </div>
+                                                {feature}
+                                            </li>
+                                        ))}
+                                    </div>
+                                    <div
+                                        href={tier.href}
+                                        aria-describedby={tier.id}
+                                        className={classNames('bg-black font-["Semibold"] text-white', 'w-full rounded-full mt-4 text-sm flex items-center justify-center py-3')}
+                                    >
+                                        Join Today
+                                    </div>
                                 </div>
-                            </TabPanel>
-                        </TabGroup>
+                            ))}
+                        </div>
+
+
                     </div>
+                    <div class='mt-20'>
+                        <div class='font-["Semibold"] text-2xl'>Frequently asked questions</div>
+                        <div class='grid md:grid-cols-2 mt-10 gap-20'>
+                            <div>
+                                <div class='font-["Semibold"]'>Do you offer a free trial?</div>
+                                <div class='font-["Medium"] mt-1 text-gray-400'>We dont offer a free trial although our beta stage product will have the majority off paid features within it.</div>
+                            </div>
+                            <div>
+                                <div class='font-["Semibold"]'>Do I need a credit card for the free plan?</div>
+                                <div class='font-["Medium"] mt-1 text-gray-400'>No, we wont ask for any payment information. If you were to upgrade would be the only time we ask for payment info.</div>
+                            </div>
+                            <div>
+                                <div class='font-["Semibold"]'>Are payments secure?</div>
+                                <div class='font-["Medium"] mt-1 text-gray-400'>All payments are secure, we use stripe as our payment gateway. Learn more about stripe here.</div>
+                            </div>
+                            <div>
+                                <div class='font-["Semibold"]'>Is there a anual payment plan?</div>
+                                <div class='font-["Medium"] mt-1 text-gray-400'>Being this early with only a beta stage, we do not offer a annual plan as of yet.</div>
 
+                            </div>
+                        </div>
+                    </div>
+                    <div class='mt-20 w-full'>
+                        <div class='bg-gray-100 flex items-center justify-center rounded-2xl md:py-24 py-14 md:p-14 p-8'>
+                            <div class='md:w-2/3'>
+                                <div class='text-gray-400 font-["Semibold"]'>What our users say about us</div>
+                                <div class='md:text-3xl text-xl mt-1 font-["Semibold"]'>Setting up my salon was effortless—within minutes, I was already receiving bookings!</div>
+                                <div class='flex items-center mt-3 gap-2'>
+                                    <img src={initials} class='w-10 h-10 rounded-full'/>
+                                    <div>
+                                        <div class='text-sm font-["Semibold"]'>Sarah Johnson</div>
+                                        <div class='text-sm font-["Medium"] text-gray-400'>Owner of Salon</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
             <div class="relative w-full">
                 <div class="absolute h-16 w-full bg-white rounded-b-[50px] z-10"></div>
                 <img src='/Browser.svg' />
