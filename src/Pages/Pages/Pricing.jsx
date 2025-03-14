d41d99a0a01e2c19049bd4ce39e813fec16f44a1
zeke <koteer212@gmail.com>
2025-03-13 23:03:01 -0500

    djfk

--- src/Pages/Pages/Pricing.jsx
+++ src/Pages/Pages/Pricing.jsx
@@ -80,18 +80,10 @@ export function Pricing() {
         <div>
             <div class='lg:max-w-screen-lg lg:mx-auto mx-5 flex items-center justify-center flex-col'>
                 <Header />
-                <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-                    <div aria-hidden="true" className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl">
-                        <div
-                            style={{
-                                clipPath:
-                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                            }}
-                            className="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-                        />
-                    </div>
+                <div className="relative isolate px-6 py-24 sm:py-32 lg:px-8">
+                 
                     <div className="mx-auto max-w-4xl text-center">
-                        <p className="mt-2 text-5xl font-['Semibold'] tracking-tight text-balance text-black sm:text-6xl">
+                        <p className="md:mt-2 mt-8 text-5xl font-['Semibold'] tracking-tight text-balance text-black sm:text-6xl">
                             Simple Pricing
                         </p>
                         <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-['Medium'] text-gray-500">
@@ -183,7 +175,7 @@ export function Pricing() {
                                 <div class='text-gray-400 font-["Semibold"]'>What our users say about us</div>
                                 <div class='md:text-3xl text-xl mt-1 font-["Semibold"]'>Setting up my salon was effortless—within minutes, I was already receiving bookings!</div>
                                 <div class='flex items-center mt-3 gap-2'>
-                                    <img src={initials} class='w-10 h-10 rounded-full'/>
+                                    <img src={initials} class='w-10 h-10 rounded-full' />
                                     <div>
                                         <div class='text-sm font-["Semibold"]'>Sarah Johnson</div>
                                         <div class='text-sm font-["Medium"] text-gray-400'>Owner of Salon</div>
--- src/components/assets/Group3.jsx
+++ src/components/assets/Group3.jsx
@@ -1,8 +1,16 @@
 export default function Group3({ className = "" }) {
     return (
         <div className={`${className} rounded-full`}>
-            <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M28.7 0H12.8L0 12.8V28.7L12.8 41.5H28.7L41.5 28.7V12.8L28.7 0ZM15 28.9L6.80002 20.7L15 12.5C18.1 9.4 23.2 9.4 26.3 12.5L34.5 20.7L26.3 28.9C23.2 32 18.2 32 15 28.9Z" fill="white" />
+            <svg width="100%" height="100%" viewBox="0 0 98 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="98" height="100" rx="30" fill="#fff" />
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M29 50L29 34C29 31.7909 30.7909 30 33 30L49.0053 30L48.9977 30C48.8856 30.0011 44.652 30.0904 36.8788 37.5758C29.0002 45.1625 29 49.9997 29 50ZM60.8182 62.4242C69 54.8485 69 50 69 50V66C69 68.2091 67.2091 70 65 70V70L49 70C49 70 52.6364 70 60.8182 62.4242ZM65 30V30C61.1583 30 57.4739 31.5261 54.7573 34.2427L33.2426 55.7573C30.5261 58.4739 29 62.1583 29 66V66C29 68.2091 30.7909 70 33 70V70C36.8318 70 40.5049 68.4695 43.203 65.7485L64.9634 43.8038C67.5491 41.1961 69 37.6724 69 34V34C69 31.7909 67.2091 30 65 30Z" fill="#000" />
+                <defs>
+                    <linearGradient id="paint0_linear_5002_2250" x1="29" y1="30" x2="69" y2="70" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" />
+                        <stop offset="0.5" stop-color="#F2F2F7" />
+                        <stop offset="1" stop-color="#F2F2F7" />
+                    </linearGradient>
+                </defs>
             </svg>
 
         </div>
