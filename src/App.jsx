630a6c4bfe0b591996129477ae89eff8e90cee13
zeke <koteer212@gmail.com>
2025-04-22 14:37:02 -0500

    djfk

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ import { setContext } from "apollo-link-context";
 import { Setup } from "./Pages/Auth/Setup/Setup";
 import { Setup2 } from "./Pages/Auth/Setup/Setup2";
 import { Setup3 } from "./Pages/Auth/Setup/Setup3";
-import { Analytics } from '@vercel/analytics'
+import { Analytics as Ana } from '@vercel/analytics/react'
 import Buy from "./Pages/Buy2";
 import { Products } from "./Pages/Dash/Products";
 import { Orders } from "./Pages/Dash/Orders";
 import { Bookings } from "./Pages/Dash/Booking";
-import { Analytics as Ana } from "./Pages/Dash/Analytics";
-import { Pricing } from './Pages/Pages/Pricing'
+import { Analytics } from "./Pages/Dash/Analytics";
+import { Pricing} from './Pages/Pages/Pricing'
 import { Pages } from "./Pages/Dash/Pages";
 import { Storefronts } from "./Pages/Pages/Storefronts";
 import { Workshops } from "./Pages/Pages/Workshops";
@@ -26,7 +26,7 @@ import { BuilderForm } from "./Pages/Pages/BuilderForm";
 import { Form } from "./Pages/Pages/Form";
 import { Storefront } from "./Pages/Viewers/Storefront";
 import { Linkinbio } from "./Pages/Viewers/Linkinbio";
-import { Linkinbio as LinkBuilder } from "./Pages/Pages/Linkinbio";
+import { Linkinbio as LinkBuilder} from "./Pages/Pages/Linkinbio";
 import { Insights } from "./Pages/Pages/Insights";
 import { Beta } from "./components/Beta";
 import { Confirmation } from "./components/Confirmation";
@@ -38,12 +38,12 @@ function App() {
   const httpLink = new HttpLink({
     uri: "http://localhost:4000/", // Replace with your GraphQL API URI
   });
-
+  
   // Create an Auth Link to include the Authorization header
   const authLink = setContext(async (_, { headers }) => {
     // Get the token from localStorage
     const token = localStorage.getItem("token");
-
+  
     // Return the headers with the Authorization header
     return {
       headers: {
@@ -52,58 +52,57 @@ function App() {
       },
     };
   });
-
+  
   // Combine the authLink and httpLink
   const link = authLink.concat(httpLink);
-
+  
   // Create the Apollo Client instance with cache and link
   const client = new ApolloClient({
     link: link,
     cache: new InMemoryCache(),
   });
-
+  
 
 
 
 
   return (
     <ApolloProvider client={client}>
-      <Analytics>
-        <Router>
-          <Routes>
-            <Route element={<LandingPage />} path="/" />
-            <Route element={<Register />} path="/register" />
-            <Route element={<SignIn />} path='/login' />
-            <Route element={<Authenticated><Buy /></Authenticated>} path='/buy' />
-            <Route element={<Authenticated><Form /></Authenticated>} path='/forms' />
-            <Route element={<Authenticated><Workshops /></Authenticated>} path='/workshops' />
-            <Route element={<Authenticated><LinkBuilder /></Authenticated>} path='/linkinbio' />
-            <Route element={<Authenticated><Storefronts /></Authenticated>} path='/storefronts' />
-            <Route element={<Authenticated><Pages /></Authenticated>} path='/pages' />
-            <Route element={<Beta />} path='/beta' />
-            <Route element={<Authenticated><Builder /></Authenticated>} path='/editor' />
-            <Route element={<Pricing />} path='/pricing' />
-            <Route element={<Confirmation />} path='/confirmation' />
-            <Route element={<Authenticated><BuilderForm /></Authenticated>} path='/builder/:type' />
-            <Route element={<Authenticated><Setup /></Authenticated>} path='/setup' />
-            <Route path='/storefront' element={
-              <Storefront />
-            } />
-            <Route path='/:subdomain' element={<Linkinbio />} />
-            <Route element={<Authenticated><Insights /></Authenticated>} path='/insights' />
-            <Route element={<Authenticated><Page /></Authenticated>} path='/page' />
-            <Route element={<Authenticated><Products /></Authenticated>} path='/products' />
-            <Route element={<Authenticated><Ana /></Authenticated>} path='/stats' />
-            <Route element={<Authenticated><Bookings /></Authenticated>} path='/bookings' />
-            <Route element={<Authenticated><Orders /></Authenticated>} path='/orders' />
-            <Route element={<Authenticated><Setup2 /></Authenticated>} path='/setup2' />
-            <Route element={<Authenticated><Audience /></Authenticated>} path='/audience' />
-            <Route element={<Authenticated><Setup3 /></Authenticated>} path='/setup3' />
-            <Route element={<Authenticated><Default /></Authenticated>} path='/dashboard' />
-            <Route element={<Authenticated><Settings /></Authenticated>} path='/settings' />
-          </Routes>
-        </Router>
-      </Analytics>
+      <Ana />
+      <Router>
+        <Routes>
+          <Route element={<LandingPage />} path="/" />
+          <Route element={<Register />} path="/register" />
+          <Route element={<SignIn />} path='/login' />
+          <Route element={<Authenticated><Buy /></Authenticated>} path='/buy'/>
+          <Route element={<Authenticated><Form /></Authenticated>} path='/forms'/>
+          <Route element={<Authenticated><Workshops /></Authenticated>} path='/workshops'/>
+          <Route element={<Authenticated><LinkBuilder /></Authenticated>} path='/linkinbio'/>
+          <Route element={<Authenticated><Storefronts /></Authenticated>} path='/storefronts'/>
+          <Route element={<Authenticated><Pages /></Authenticated>} path='/pages'/>
+          <Route element={<Beta />}  path='/beta' />
+          <Route element={<Authenticated><Builder /></Authenticated>} path='/editor'/>
+          <Route element={<Pricing />} path='/pricing'/>
+          <Route element={<Confirmation />} path='/confirmation'/>
+          <Route element={<Authenticated><BuilderForm /></Authenticated>} path='/builder/:type'/>
+          <Route element={<Authenticated><Setup /></Authenticated>} path='/setup'/>
+          <Route path='/storefront' element={
+            <Storefront />
+        } />
+        <Route path='/:subdomain' element={<Linkinbio/>}/>
+        <Route element={<Authenticated><Insights /></Authenticated>} path='/insights' />
+          <Route element={<Authenticated><Page /></Authenticated>} path='/page'/>
+          <Route element={<Authenticated><Products /></Authenticated>} path='/products'/>
+          <Route element={<Authenticated><Analytics /></Authenticated>} path='/stats'/>
+          <Route element={<Authenticated><Bookings /></Authenticated>} path='/bookings'/>
+          <Route element={<Authenticated><Orders/></Authenticated>} path='/orders'/>
+          <Route element={<Authenticated><Setup2 /></Authenticated>} path='/setup2'/>
+          <Route element={<Authenticated><Audience /></Authenticated>} path='/audience'/>
+          <Route element={<Authenticated><Setup3 /></Authenticated>} path='/setup3'/>
+          <Route element={<Authenticated><Default /></Authenticated>} path='/dashboard' />
+          <Route element={<Authenticated><Settings /></Authenticated>} path='/settings'/>
+        </Routes>
+      </Router>
     </ApolloProvider>
   )
 }
