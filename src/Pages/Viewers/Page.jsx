9e33a09f6fd5a380be1cab87fc338542c5c3881c
zeke <koteer212@gmail.com>
2025-05-04 22:39:53 -0500

    dhjd

--- src/Pages/Viewers/Page.jsx
+++ src/Pages/Viewers/Page.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { BackdropSimple, Button, Description, Simple } from '../../Pages/Pages/Builder';
 import { EllipsisVertical } from 'lucide-react';
 import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
+import verified from '../../components/assets/twitterverified.png'
 const GET_LINK_BY_SUBDOMAIN = gql`
   query storeBySubdomain($subdomain: String!) {
     storeBySubdomain(subdomain: $subdomain) {
@@ -21,6 +22,7 @@ const GET_LINK_BY_SUBDOMAIN = gql`
       headerImage
       user{
       id
+      verified
       Links{
       id
       linkText
@@ -68,6 +70,9 @@ export const Linkinbio = () => {
 
     return (
         <div class='flex flex-col min-h-screen px-5 relative max-w-md mx-auto h-full w-full'>
+            <div class='absolute top-5 left-5 rounded-full text-white bg-black bg-opacity-10 text-sm px-5 py-2 font-["Semibold"]'>
+                {store?.user?.verified === true ? 'Verified' : 'user'}
+            </div>
             <div class='absolute top-5 right-5 rounded-full flex items-center justify-center w-10 h-10 bg-black bg-opacity-10'>
                  <EllipsisHorizontalIcon class='w-4 h-4 text-white' />
             </div>
@@ -75,8 +80,8 @@ export const Linkinbio = () => {
                 <img src={store?.headerImage} style={{ boxShadow: store?.backdrop && '4px 4px 0px 0px'}} class={`w-28 h-28 ${store?.backdrop && ('border-2 border-black')} rounded-full`} />
 
                 <div class='text-center mt-6'>
-                    <div class='text-xl text-black font-["Semibold"]' style={{}}>{store?.headerText}</div>
-                    <div class='text-gray-400'>{store?.description}</div>
+                    <div class='text-xl text-black flex items-center gap-1 font-["Semibold"]' style={{}}>{store?.headerText} <img src={verified} class={`${store?.user?.verified === true ? 'w-6 h-6' : 'hidden'}`} /></div>
+                    <div class='text-gray-400 text-center max-w-sm mt-1'>{store?.description}</div>
                 </div>
                 <div class='mt-5 w-full'>
                     <div class={`${store?.storefront === true ? 'flex' : 'hidden'}`}>
