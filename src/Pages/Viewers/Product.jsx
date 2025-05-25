679b5a423192e4abd754f6856576f400656494b0
zeke <koteer212@gmail.com>
2025-05-24 22:22:06 -0500

    jkdf

--- src/Pages/Viewers/Product.jsx
+++ src/Pages/Viewers/Product.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
 import { useParams } from 'react-router-dom';
 
@@ -26,36 +26,44 @@ const ProductOne = () => {
         variables: { slug: slug }
     })
 
+    const [process, setProcess] = useState({
+        product: true,
+        shipping: false,
+        payment: false
+    })
+
     const product = data?.getProduct
 
     if (loading) return <div>Loading...</div>
     if (error) return <div>{error.message}</div>
     return (
         <div class='h-full'>
-            <img src={data?.getProduct?.thumbnail} class='bg-black' />
-            <div class='mt-[-50px] bg-white w-full h-full flex p-7 flex-col z-20 relative rounded-t-3xl'>
-                <div class='text-2xl font-["Semibold"]'>{data?.getProduct?.title}</div>
-                <div class='text-sm font-["Medium"] text-gray-500'>{data?.getProduct?.description}</div>
-                <div class='mt-7 flex self-stretch justify-between'>
-                    <div class='w-full'>
-                        <div class='text-sm font-["Semibold"]'>Sizes</div>
-                        <div class='flex items-cneter gap-1 mt-2 flex-wrap'>
-                            {product.sizes.map(item => (<div class='text-xs font-["Semibold"] px-3 py-1 rounded-full border'>{item}</div>))}
+            <div class={`${process.product === true ? 'relative' : 'hidden'}`}>
+                <img src={data?.getProduct?.thumbnail} class='bg-black' />
+                <div class='mt-[-50px] bg-white w-full h-full flex p-7 flex-col z-20 relative rounded-t-[50px]'>
+                    <div class='text-2xl font-["Semibold"]'>{data?.getProduct?.title}</div>
+                    <div class='text-2xl font-["Semibold"]'>${product.price}</div>
+                    <div class='text-sm font-["Medium"] text-gray-500'>{data?.getProduct?.description}</div>
+                    <div class='mt-7 flex self-stretch justify-between'>
+                        <div class='w-full'>
+                            <div class='text-sm font-["Semibold"]'>Sizes</div>
+                            <div class='flex items-cneter gap-1 mt-2 flex-wrap'>
+                                {product.sizes.map(item => (<div class='text-xs font-["Semibold"] px-3 py-1 rounded-full border'>{item}</div>))}
+                            </div>
+                        </div>
+                        <div class='w-full'>
+                            <div class='text-sm font-["Semibold"]'>Colors</div>
+                            <div class='flex items-cneter gap-1 mt-2 flex-wrap'>
+                                {product.colors.map(item => (<div class='text-xs font-["Semibold"] flex items-center gap-2 px-2 py-1 rounded-full border'><div style={{ backgroundColor: item }} class='w-3 h-3 rounded-full border' />{item}</div>))}
+                            </div>
                         </div>
                     </div>
-                    <div class='w-full'>
-                        <div class='text-sm font-["Semibold"]'>Colors</div>
-                        <div class='flex items-cneter gap-1 mt-2 flex-wrap'>
-                            {product.colors.map(item => (<div class='text-xs font-["Semibold"] flex items-center gap-2 px-2 py-1 rounded-full border'><div style={{ backgroundColor: item }} class='w-3 h-3 rounded-full border'/>{item}</div>))}
-                        </div>
-                    </div>
+
                 </div>
-            
+                <div class='flex items-center z-40 bottom-0 gap-3 px-7 py-3 fixed w-full bg-white'>
+                    <button class='w-full bg-black py-3 rounded-full text-white text-sm font-["Semibold"]'>Purchase</button>
+                </div>
             </div>
-                <div class='flex items-center z-40 bottom-0 gap-3 p-2 fixed w-full bg-white border-t'>
-                       <button class='w-full py-3 rounded-full font-["Semibold"] text-sm border'>Back</button>
-                       <button class='w-full bg-black py-3 rounded-full text-white text-sm font-["Semibold"]'>Purchase</button>
-                </div>
         </div>
     )
 }
